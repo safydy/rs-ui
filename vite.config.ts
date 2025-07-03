fae8c05715b16b2c024f21f9b3386106451855b2
safydy <r.safydy@gmail.com>
2025-07-03 16:06:09 +0300

    fix npm build

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,33 @@
 import { defineConfig } from 'vite';
+import type { UserConfigExport } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import path from 'path';
 import dts from 'vite-plugin-dts';
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    dts({ include: ['src'] }),
+    dts({
+      include: [
+        "src/components/**/*.ts",
+        "src/components/**/*.tsx",
+        "src/main.tsx"
+      ],
+      exclude: [
+        '**/*.test.ts',
+        '**/*.test.tsx',
+        'src/App.tsx',
+        '**/stories/**',
+        'src/test/**'
+      ]
+    }),
   ],
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
+      entry: path.resolve(__dirname, 'src/main.tsx'),
       name: 'RsUI',
       fileName: (format) => `rs-ui.${format}.js`,
     },
@@ -28,10 +42,11 @@ export default defineConfig({
       },
     },
   },
+  // Configure Vitest
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/test/setup.ts'],
     css: true,
   }
-});
+} as UserConfigExport);
